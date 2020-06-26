6c42132289d3a8b94fec64c68107c541b540385d
andré felipe <andre@andrefelipe.com>
2020-06-26 17:33:36 -0300

    update version

--- html/app/themes/bond/functions.php
+++ html/app/themes/bond/functions.php
@@ -1,10 +1,11 @@
 <?php
 
 // Forward to our root theme
-// IMPORTANT: get_stylesheet below implies the theme has the same folder name
-
 $root_app = defined('ROOT_APP_PATH')
     ? ROOT_APP_PATH
     : dirname($_SERVER['DOCUMENT_ROOT']) . '/app';
 
 require_once $root_app . '/themes/' . get_stylesheet() . '/functions.php';
+
+// Note: get_stylesheet below implies the theme has the same folder name
+// but you can change if you want
