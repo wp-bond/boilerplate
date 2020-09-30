4b1f837e5e6f2d4b41808f01284257b097b0d2e9
andré felipe <andre@andrefelipe.com>
2020-09-30 16:21:50 -0300

    force the usage of ROOT_APP_PATH to not rely on SERVER document root (fix WP CLI)

--- html/app/themes/bond/functions.php
+++ html/app/themes/bond/functions.php
@@ -1,11 +1,7 @@
 <?php
 
 // Forward to our root theme
-$root_app = defined('ROOT_APP_PATH')
-    ? ROOT_APP_PATH
-    : dirname($_SERVER['DOCUMENT_ROOT']) . '/app';
+require_once ROOT_APP_PATH . '/themes/' . get_stylesheet() . '/functions.php';
 
-require_once $root_app . '/themes/' . get_stylesheet() . '/functions.php';
-
-// Note: get_stylesheet below implies the theme has the same folder name
+// Note: get_stylesheet above implies the theme has the same folder name
 // but you can change if you want
