c167d6329e0ea03b855f3c7fb01193fa48ebcac6
andré felipe <andre@andrefelipe.com>
2021-03-01 08:50:50 -0300

    some attachment methods

--- app/themes/boilerplate/app/Post/Attachment.php
+++ app/themes/boilerplate/app/Post/Attachment.php
@@ -3,11 +3,26 @@
 namespace App\Post;
 
 use Bond\Support\Fluent;
+use Bond\Utils\Image;
 
 class Attachment extends BasePost
 {
     public string $post_type = ATTACHMENT;
 
+    public function link(string $language_code = null): string
+    {
+        return Image::url($this->ID, 'original');
+    }
+
+    public function caption(): string
+    {
+        return Image::caption($this->ID);
+    }
+
+    public function alt(): string
+    {
+        return Image::alt($this->ID);
+    }
 
     public function values(string $for = ''): Fluent
     {
@@ -17,6 +32,11 @@ class Attachment extends BasePost
         switch ($for) {
 
             case 'api':
+                $values->id = $this->ID;
+                $values->imageTag = Image::pictureTag(
+                    $this->ID,
+                    THUMBNAIL
+                );
                 break;
 
             default:
