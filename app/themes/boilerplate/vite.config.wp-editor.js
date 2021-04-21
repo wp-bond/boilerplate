8c2654b5d667f301c722257289a8c18b3e61f34f
andré felipe <andre@andrefelipe.com>
2021-04-21 12:31:18 -0300

    wp editor build watch

--- app/themes/boilerplate/vite.config.wp-editor.js
+++ app/themes/boilerplate/vite.config.wp-editor.js
@@ -1,13 +1,13 @@
 import { default as config, themeDir, themeUrl } from './vite.config.js'
 
-config.server.port = 3001
 config.build.rollupOptions.input = '/wp/editor.js'
 config.build.outDir = themeDir + '/dist-wp-editor'
-config.base = process.env.APP_ENV === 'development'
-  ? '/'
-  : themeUrl + '/dist-wp-editor/'
+config.base = themeUrl + '/dist-wp-editor/'
 
 // not needed, we just want the css
 delete config.plugins
+
+// Note: live reload doesn't work in this case
+// have to refresh the WP admin manually after build
 
 export default config
