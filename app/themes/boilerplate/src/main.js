327bd932ff8fffec356a273827718b73fc97c33f
andré felipe <andre@andrefelipe.com>
2021-11-09 16:11:05 -0300

    updated with auto import

--- app/themes/boilerplate/src/main.js
+++ app/themes/boilerplate/src/main.js
@@ -1,18 +1,45 @@
-import 'vite/dynamic-import-polyfill'
+// Styles
+import './styles'
+
+// Vue
 import { createApp } from 'vue'
 
-// Styles
-import './style'
+// https://vitejs.dev/config/#build-polyfillmodulepreload
+// using workaround https://github.com/vitejs/vite/issues/4786
+if (import.meta.env.MODE !== 'development') {
+  import('vite/modulepreload-polyfill')
+}
 
-// in-DOM HTML Components
-import HelloWorld from './components/HelloWorld.vue'
+// If you are build a SPA with a single <div id="app"></div> entry you would:
+// import App from './App.vue'
+// createApp(App).mount('#app')
+
+// The example here is to have multiple Vue apps sprinkled throughout your page
+// So we would instantiate any known components by their own
+
+// First let's load all components that should be available to in-browser template compilation
+
+// Example of how to import **all** components
+// if importing all is too much you can always do it manually
+// https://dev.to/jakedohm_34/auto-registering-all-your-components-in-vue-3-with-vite-4884
+
+const modules = import.meta.globEager('./components/*.vue')
+const components = {}
+for (const path in modules) {
+  const name = path
+    .split('/')
+    .pop()
+    .replace(/\.\w+$/, '')
+
+  components[name] = modules[path].default
+}
+
+// instantiate the Vue apps
+// Note our lookup is a wrapping div with .vue-app class
 
 for (const el of document.getElementsByClassName('vue-app')) {
   createApp({
-    components: {
-      HelloWorld
-      // add more if needed
-    },
-    template: el.innerHTML
+    components,
+    template: el.innerHTML,
   }).mount(el)
 }
